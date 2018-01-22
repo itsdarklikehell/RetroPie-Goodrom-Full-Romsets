dd88a75f4d8b87b22e3c49a2f4a877742b8eac11
Bauke Molenaar <bauke.molenaar@gmail.com>
2018-01-22 16:21:39 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+echo "This script will download and extract the selected GOODROM set to its corresponding directories in your rom folder."
+CONFIG(){
+	ROMDIR="~/RetroPie/roms"
+	WORKDIR="~/RetroPie-Goodrom-Full-Romsets/Romsets"
+	echo "Romdir is set to :" $ROMDIR
+	echp "Workdir is set to :" $WORKDIR
+	}
+CONFIG
+
